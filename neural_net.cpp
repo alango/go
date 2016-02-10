ba79c814f6acd40db03d75577e7ca2ce2190f06b
Alan Go <alan.go@live.com>
2016-02-10 11:42:05 +0000

    Removed debugging statements

--- neural_net.cpp
+++ neural_net.cpp
@@ -25,7 +25,6 @@ double Neuron::process_inputs(std::vector<int> inputs)
 		activation += inputs[i] * weights[i];
 	}
 	// Apply sigmoid function to activation level.
-  // std::cout << "hidden layer output: " << 1 / (1 + exp(-activation)) << std::endl;
 	return 1 / (1 + exp(-activation));
 }
 
@@ -76,15 +75,12 @@ void NeuralNet::update_weights(std::vector<int> inputs, double target)
 	double error = output - target;
   std::cout << "error: " << error << std::endl;
   double d_error_d_activation = error * output * (1-output);
-  // std::cout << "d_error_d_activation: " << d_error_d_activation <<std::endl;
   std::vector<double> new_output_neuron_weights;
   double delta;
   double new_weight;
   for (int i = 0; i != hidden_layer_size; i++)
   {
-    // std::cout << "hidden_layer_outputs: " << hidden_layer_outputs[i] << std::endl;
     delta = d_error_d_activation * hidden_layer_outputs[i];
-    // std::cout << "delta: " << delta << std::endl;
     new_weight = output_neuron.weights[i] - (learning_rate*delta);
     new_output_neuron_weights.push_back(new_weight);
   }
@@ -94,7 +90,6 @@ void NeuralNet::update_weights(std::vector<int> inputs, double target)
   {
     delta = d_error_d_activation * output_neuron.weights[i] * hidden_layer_outputs[i]
             * (1-hidden_layer_outputs[i]);
-    // std::cout << "hidden_layer delta: " << delta << std::endl;
     // Iterate through each weight in the neuron, including the bias.
     for (int j = 0; j < inputs.size(); j++)
     {
